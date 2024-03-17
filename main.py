d2b0cc4a2ad8052597c09ed4e4e44668732de732
White <white@chbrath.de>
2024-03-17 11:08:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import base64
+from flask import Flask, request, json
+from waitress import serve
+
+app = Flask(__name__)
+
+list_decode = lambda inp: [x for x in base64.b64decode(inp).decode().split("#")]
+
+
+@app.route("/get/<id>", methods=["GET"])
+def get(id):
+    try:
+        out = list_decode(id)
+    except:
+        return "error", 400
+    with open("flask_server/default.html", "r") as f:
+        html_raw = f.read()
+    ans = ""
+    for i, v in enumerate(out, 1):
+        if not "[" in v:
+            ans += f"{i}.&nbsp;&nbsp;{v}<br>"
+        elif "[" in v:
+            tmp = eval(v)
+            ans += f"{i}. <br>"
+            for j, w in enumerate(tmp, 0):
+                ans += f"&nbsp;&nbsp;{chr(j+ord('a'))})&nbsp;&nbsp;{w}<br>"
+
+    html_raw = html_raw.replace("{%loesung%}", ans)
+    return html_raw, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # serve(app, host="127.0.0.1", port=5000)
